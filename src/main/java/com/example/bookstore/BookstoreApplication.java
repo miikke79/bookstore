f12d7c1b8cdb3c35e42c22a33b8a5d83ad70138f
Michael Brown <MY_NAME@example.com>
2021-10-04 20:52:48 +0300

    04/10 two

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -11,6 +11,10 @@ import com.example.bookstore.model.Book;
 import com.example.bookstore.model.Bookrepository;
 import com.example.bookstore.model.Category;
 import com.example.bookstore.model.Categoryrepository;
+import com.example.bookstore.model.User;
+import com.example.bookstore.model.UserRepository;
+
+
 
 
 
@@ -25,7 +29,7 @@ public class BookstoreApplication {
     }
     
     @Bean
-    public CommandLineRunner bookDemo(Bookrepository repository, Categoryrepository crepository) {
+    public CommandLineRunner bookDemo(Bookrepository repository, Categoryrepository crepository, UserRepository urepository) {
         return (args) -> {
             log.info("save a couple of books");
             crepository.save(new Category("Horror"));
@@ -35,6 +39,11 @@ public class BookstoreApplication {
             repository.save(new Book("Cool Drink", "Kris Pepper", 1990, "0-3333-9961-1", 24.99, crepository.findByName("Comedy").get(0)));
             repository.save(new Book("Hot Egg", "John McGinn", 1980, "0-9870-9920-4", 19.99, crepository.findByName("Horror").get(0)));    
             
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+			
             log.info("fetch all books");
             for (Book book : repository.findAll()) {
                 log.info(book.toString());
